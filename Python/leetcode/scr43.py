41742526efe5497faa48dd60f85b1c5ca74a9c90
danericpelayo <daneric.pelayo@gmail.com>
2023-05-28 14:13:20 -0400

    Sun May 28 14:13:20 EDT 2023

--- Python/leetcode/scr43.py
+++ Python/leetcode/scr43.py
@@ -1,31 +1,27 @@
-# THIS CODE IS NOT YET COMPLETE
+def str_to_int(str_num):
+    dict_snums={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+    int_multiplier=1
+    int_num=0
+    for chr_num in str_num[::-1]:
+        int_num+=(dict_snums.get(chr_num)*int_multiplier)
+        int_multiplier*=10
+    return int_num
+
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == 0 or num2 == 0 or num1 == "" or num2 == "":
+        if num1 == 0 or num2 == 0:
             return "0"
         else:
-            dict_snums={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
             dict_nstrs={0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
-            int_multiplier=1
-            int_num1=0
-            for chr_num in num1[::-1]:
-                int_num1+=(dict_snums.get(chr_num)*int_multiplier)
-                int_multiplier*=10
-            int_multiplier=1
-            int_num2=0
-            for chr_num in num2[::-1]:
-                int_num2+=(dict_snums.get(chr_num)*int_multiplier)
-                int_multiplier*=10
-            int_multiplier=10
-            int_product=int_num1*int_num2
-            str_product=""
-            print(int_product)
+            int_product=str_to_int(num1)*str_to_int(num2)
+            list_product=[]
             while True:
-                str_product+=dict_nstrs.get(((int_product%int_multiplier)-(int_product%(int_multiplier/10)))//(int_multiplier/10))
-                int_multiplier*=10
-                if int(str_product[::-1])==int_product:
+                # str_product+=dict_nstrs.get(((int_product%int_multiplier)-(int_product%(int_multiplier/10)))//(int_multiplier/10))
+                list_product.insert(len(list_product),dict_nstrs.get(int_product%10))
+                int_product=int_product//10
+                if int_product==0:
                     break
-            return str_product[::-1]
+            return "".join(list_product)[::-1]
 
 obj_test = Solution()
-obj_test.multiply('648383427', '93073533')
+print(obj_test.multiply('648383427', '93073533'))
