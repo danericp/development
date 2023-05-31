0300acebdf18256a51c1fb4409fb454a8b7a5b60
danericpelayo <daneric.pelayo@gmail.com>
2023-05-31 19:05:45 -0400

    Wed May 31 19:05:45 EDT 2023

--- Python/leetcode/scr6.py
+++ Python/leetcode/scr6.py
@@ -1,55 +1,27 @@
-# THIS CODE IS NOT YET COMPLETE
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
         list_builder=[]
         int_ctr=0
         bool_xadd=False
-        while int_ctr <= len(s):
-            list_builder.append((','*(numRows-1)).split(','))
-            int_ctr+=numRows
-        x, y = 0, numRows-1
+        while int_ctr < numRows:
+            list_builder.append((','*((len(s)//2))).split(','))
+            int_ctr+=1
+        x, y = 0, 0
+        int_ctr=0
         while int_ctr < len(s):
             list_builder[x][y]=s[int_ctr]
             if bool_xadd == False:
-                y-=1
-                bool_xadd = True if y == 0 else False
-            else:
                 x+=1
+                bool_xadd = True if x == numRows-1 else False
+            else:
+                x-=1
                 y+=1
-                bool_xadd = False if y == numRows-1 else True
+                bool_xadd = False if x == 0 else True
             int_ctr+=1
-        print(list_builder)
-        return "asd"
-
-'''
-
-P  I  N
-A LS IG
-YA HR
-P  I
-
-[
-    ['N', 'H', 'A', 'P'], 
-    [' ', 'A', ' ', ' '], 
-    [' ', ' ', 'P', ' '], 
-    ['I', 'I', 'S', 'L']
-]
-
-14//4=3
-
-14          P
-13          A
-12          Y
-11          P
-32          A
-53          L
-74          I
-73          S
-72          H
-71          I
-92          R
-11,3        I
-13,4        N
-13,3        G
-
-'''
+        # print(list_builder)
+        return "".join(["".join(a) for a in list_builder])
+str1='PAYPALISHIRING'
+obj_Solution = Solution()
+print(obj_Solution.convert(str1, 3))
